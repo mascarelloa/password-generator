2e5bd313bab6feb42dc1c5f8ef2e48257b1c4d6e
mascarelloa <amfm0793@gmail.com>
2021-03-26 16:33:49 -0400

    adding final touches and formatting code.

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,6 @@ var lowers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', '
 var uppers = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -27,21 +26,24 @@ function generatePassword() {
   var characterPool = [];
   // Ask user how many characters they want.
   var passwordLength = prompt("How many characters do you want your password to be? Enter a number between 8 and 128");
+  // Alert user they must input a valid number between 8-128.
+  if (isNaN(parseInt(passwordLength))) {
+    alert("Wait! You must enter a valid number between 8 and 128.");
+    return "Try again.";
 
-  // Alert user they must input 8-128.
-  if (passwordLength > 128 || passwordLength < 8) {
+  } else if (passwordLength > 128 || passwordLength < 8) {
     alert("Wait! Your password must be at least 8 characters and no more than 128 characters.");
     return "Try again.";
     // When the user inputs a valid number, continue...
   } else {
-    
+
     // Ask what kind of characters they want in the password.
     var containCharacters = confirm("Do you want your password to contain special characters?");
     var containNumbers = confirm("Do you want your password to contain numbers?");
     var containLowers = confirm("Do you want your password to contain lowercase letters?");
     var containUppers = confirm("Do you want your password to contain uppercase letters?");
 
-    
+
     // If yes to special characters add special characters array to character pool.
     if (containCharacters) {
       characterPool = characterPool.concat(specials);
@@ -64,12 +66,12 @@ function generatePassword() {
 
     // Testing character pool array.
     console.log(characterPool)
-    
+
     // Creating the string containing the randomly pulled characters.
     var finalPool = "";
 
     for (var i = 0; i < passwordLength; i++) {
-      var index = Math.floor(Math.random() * characterPool.length); 
+      var index = Math.floor(Math.random() * characterPool.length);
       finalPool = finalPool + characterPool[index]
     }
 
